ee48a591fd13553b6742021b04feec834809bd0e
OrdainedRat <marcusjuniusbrutus213@gmail.com>
2022-01-20 01:50:10 -0600

    add scheme completed

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -41,6 +41,8 @@ const validateScheme = (req, res, next) => {
     typeof scheme_name !== 'string' ||
     !scheme_name.trim()) {
       next({ status: 400, message: 'invalid scheme_name'})
+    } else {
+      next()
     }
 }
 
